f6d6eb6fb66d2b42427f42514b388eec0ad33cc6
xhsioi <73170437+xhsioi@users.noreply.github.com>
2023-03-30 19:32:12 +0800

    Add files via upload

---
+++ control.py
@@ -0,0 +1,42 @@
+import numpy as np
+import sys
+import math
+
+
+def robot_control(order, _id, value=None):  # 定义了一个名为robot_control的函数，输入指令，机器人id ，参数即可操控机器人
+    if value is not None:
+        sys.stdout.write("{} {} {}\n".format(order, _id, value))
+    else:
+        sys.stdout.write("{} {}\n".format(order, _id))
+
+
+def control_to_goal(Single_robot, path, bot0_status: object):
+    target1 = np.array([path[bot0_status][0], path[bot0_status][1]])
+    # print(target1, file=sys.stderr)
+    # print(Single_robot.pos, file=sys.stderr)
+    direction1 = (target1 - Single_robot.pos) / np.linalg.norm(target1 - Single_robot.pos)  # 计算机器人到目标点的方向向量
+    direction_right1 = math.atan2(direction1[1], direction1[0])  # 机器人到目标点的夹角
+    ori = Single_robot.toward  # 机器人当前朝向
+    dis = np.linalg.norm(Single_robot.pos - target1)  # 计算机器人到目标点的距离
+    # print(Single_robot.pos, file=sys.stderr)
+    # print(target1, file=sys.stderr)
+    err = direction_right1 - ori  # 计算机器人到目标点的夹角和机器人当前朝向的误差
+    while err > math.pi:
+        err -= 2.0 * math.pi
+    while err < -math.pi:
+        err += 2.0 * math.pi
+    print(bot0_status,file = sys.stderr)
+    print(err,file = sys.stderr)
+
+    if dis < 0.5:
+        robot_control("rotate", Single_robot.id - 1, 0)
+        robot_control("forward", Single_robot.id - 1, 0)
+        bot0_status = bot0_status + 1
+    else:
+        if np.fabs(err) > np.pi/32:
+            robot_control("rotate", Single_robot.id - 1, err*4)
+            robot_control("forward", Single_robot.id - 1, 1.5)
+        else:
+            robot_control("forward", Single_robot.id - 1, 4)
+            robot_control("rotate", Single_robot.id - 1, 0)
+    return bot0_status
--- main.py
+++ main.py
@@ -1,11 +1,63 @@
+import math
+import sys
+
+import numpy as np
+
 from util import *
+from control import *
+
+
+def load_path_information(workstations):
+    data = np.load('map2.npz')
+    path_data = data['map2_path']
+    length = len(workstations)
+    for i in range(length):
+        for j in range(length):
+            for k in range(2):
+                if k == 0:
+                    workstations[i].buy.append(path_data[i][j][0])
+                else:
+                    workstations[i].sell.append(path_data[i][j][1])
+
+
 
 if __name__ == '__main__':
     # 获取地图(100x100数组)
     game_map_array = init()
-    print(np.size(game_map_array), file=sys.stderr)
+    #print(np.size(game_map_array), file=sys.stderr)
     ws_config = get_ws_config(game_map_array)
     wall_number, wall_config = get_wall_config(game_map_array)
+
+    bot0_status = 0
+
+    # # test
+    # path_0 = np.loadtxt('matrix.txt', dtype=float, delimiter=',')
+    # # print(path_0,file=sys.stderr)
+    #
+    # flag2 = False
+    # flag1 = False
+    # test_0 = []
+    # last_x = path_0[0][0]
+    # last_y = path_0[0][1]
+    # test_0.append([last_x, last_y])
+    # for i in range(1, len(path_0)):
+    #     if path_0[i][0] != path_0[i-1][0]:
+    #         if flag2:
+    #             flag2 = False
+    #             test_0.append([path_0[i][0],path_0[i][1]])
+    #         flag1 = True
+    #     if path_0[i][1] != path_0[i-1][1]:
+    #         if flag1:
+    #             flag1 = False
+    #             test_0.append([path_0[i][0],path_0[i][1]])
+    #         flag2 = True
+    # test_0.remove(test_0[0])
+    # #print(test_0,file=sys.stderr)
+
+    #加载地图信息
+    fid, money, workstations, bots = get_input_var()
+    load_path_information(workstations)
+    print(type(workstations[0].buy[1]),file = sys.stderr)
 
     workstations_lock = np.full((1, 1), 0)
     rob_path_information = np.full((1, 1), 0)  # 记录进货、出货的两个工作站的状态（-1：已完成操作；其他：未完成此操作）
@@ -13,6 +65,7 @@ if __name__ == '__main__':
     while True:
         # 获取信息
         fid, money, workstations, bots = get_input_var()
+        #[0.75, 48.75], [4.75, 48.25]
         # 输出 fid
         sys.stdout.write('%d\n' % fid)
 
@@ -31,7 +84,10 @@ if __name__ == '__main__':
             for i in range(len(bots)):
                 rob_startpoint[i][0] = bots[i].x
                 rob_startpoint[i][1] = bots[i].y
+        #print(bot0_status,file=sys.stderr)
+        #print(bots[0].pos,file=sys.stderr)
 
+        # bot0_status = control_to_goal(bots[0], test_0, bot0_status)
         # 结束
         sys.stdout.write('OK\n')
         sys.stdout.flush()
