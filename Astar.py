4113afbd78757f3ef5801d0d6f2ba157e4a24918
ghtll <95964686+ghtll@users.noreply.github.com>
2023-04-04 17:30:39 +0800

    Add files via upload

--- Astar.py
+++ Astar.py
@@ -8,11 +8,12 @@ Created on Wed Mar 29 16:17:43 2023
 import math
 import numpy as np
 from model import *
-#地图大小50×50米
+
+# 地图大小50×50米
 MAP_SIZE = (50, 50)
 
 
-#Node类
+# Node类
 class Node:
     def __init__(self, x, y):
         self.x = x
@@ -21,41 +22,41 @@ class Node:
         self.h = 0
         self.parent = None
         self.obstacle = False
-    
+
     def __eq__(self, other):
         return self.x == other.x and self.y == other.y
-    
+
     def __hash__(self):
         return hash((self.x, self.y))
 
 
-#A*算法
-def astar(start, goal, obstacles,R):
+# A*算法
+def astar(start, goal, obstacles):
     open_set = set()
     closed_set = set()
     current = start
     open_set.add(current)
-    counter=1
+    counter = 1
     while open_set:
-        counter+=1
-        if counter>=10000:
-            return None
+        counter += 1
+        # if counter >= 10000:
+        #     return None
         current = min(open_set, key=lambda x: x.g + x.h)
         open_set.remove(current)
         closed_set.add(current)
 
-        if get_distance(current, goal)<0.4:
+        if get_distance(current, goal) < 0.4:
             path = []
             while current:
                 path.append(current)
                 current = current.parent
             return path[::-1]
 
-        for neighbor in get_neighbors(current, obstacles,R):
+        for neighbor in get_neighbors(current, obstacles):
             if neighbor in closed_set:
                 continue
 
-            tentative_g = current.g + get_distance(current, neighbor)
+            tentative_g = get_distance(current, neighbor)
             if neighbor not in open_set:
                 open_set.add(neighbor)
                 neighbor.h = get_distance(neighbor, goal)
@@ -64,117 +65,181 @@ def astar(start, goal, obstacles,R):
 
             neighbor.parent = current
             neighbor.g = tentative_g
-    
+
     return None
 
-#判断点是否违法
-def is_valid_location(x, y, obstacles,R):
 
-    if x <= 0.25 or x >= 49.75 or y <= 0.25 or y >= 49.75: #超出边界，违法
-        return False
-    for obstacle in obstacles:
-        if (x > obstacle.x - R-0.04 and x < obstacle.x + R+0.04 and  #进入障碍物，违法
-           y > obstacle.y - R-0.04 and y < obstacle.y + R+0.04):
-            return False
-    return True
 
-#获取当前点的邻居点
-def get_neighbors(node, obstacles,R):
+
+# 获取当前点的邻居点
+def get_neighbors(node, obstacles):
     neighbors = []
-    if R==0.45+0.25:
-        K=4
-    else:
-        K=2
     for x in range(-1, 2):
         for y in range(-1, 2):
-            if x==0 and y==0:
+            if x ==0 and y==0:
                 continue
-            
-            if not is_valid_location(node.x + x/K, node.y + y/K, obstacles,R):
+            if node.x+x < 0.25 or node.x+x >49.75 or node.y+y < 0.25 or node.y+y >49.75 or Node(node.x+x/4,node.y+y/4) in obstacles:
                 continue
-            neighbor = Node(node.x + x/K, node.y + y/K)
+            neighbor = Node(node.x + x / 4, node.y + y / 4)
             neighbors.append(neighbor)
     return neighbors
 
+
 def get_distance(node1, node2):
     return math.sqrt((node1.x - node2.x) ** 2 + (node1.y - node2.y) ** 2)
-def get_manhadundis(node1,node2):
+
+
+def get_manhadundis(node1, node2):
     return abs(node1.x - node2.x) + abs(node1.y - node2.y)
 
 
-#获取起点到目标点的路径参数：地图、起点、终点、机器人是否携带商品     #main函数只调用get_astar_path函数 其他不用
-def get_astar_path(game_map_array,rob_startpoint,goal,workstations,bot,iscarry):
+# 获取起点到目标点的路径参数：地图、起点、终点、机器人是否携带商品     #main函数只调用get_astar_path函数 其他不用
+def get_astar_path(game_map_array, rob_startpoint, goal, workstations, bot, iscarry):
     print("牛牛牛\n", file=sys.stderr)
     # game_map_array,rob_startpoint,goal,workstations,bots[i]
-    obstacles=[]
-    if iscarry==0:
-        
-        R=0.25+0.45  #未携带产品，半径为机器人半径＋障碍物半径
-    else:
-        R=0.45+0.25+0.08 #携带产品，半径增加
+    obstacles = set()
     for i in range(len(game_map_array)):
         for j in range(len(game_map_array)):
-            if game_map_array[i][j]=='#':  #如果为‘#’，则为障碍物
-                obstacle=Node(0.25+(j)*0.5,(99.5-i)*0.5)
-                obstacles.append(obstacle)
+            if game_map_array[i][j] == '#':  # 如果为‘#’，则为障碍物
+                x=0.25 + j * 0.5
+                y=(99.5 - i) * 0.5
+                obstacle = Node(x, y)
+    
+                obstacles.add(obstacle)
+                if iscarry==0:
+                    for xx in range(-2,3):
+                        for yy in range(-2,3):
+                            if (xx==0 and yy==0 ):
+                                continue
+                            obstacles.add(Node(x+xx/4,y+yy/4))
                 
-    start=Node(rob_startpoint[0],rob_startpoint[1])   #起点
-    goal=Node(workstations[goal].x,workstations[goal].y)   #终点
-    path = astar(start, goal, obstacles,R)    #查找路径
-    if path:   #找到路径则返回
+                if iscarry==1:
+                    for xx in range(-3,4):
+                        for yy in range(-3,4):
+                            if (xx==0 and yy==0 or abs(xx*yy)>=6 ):
+                                continue
+                            obstacles.add(Node(x+xx/4,y+yy/4))
+
+    start = Node(rob_startpoint[0], rob_startpoint[1])  # 起点
+    goal = Node(workstations[goal].x, workstations[goal].y)  # 终点
+    path = astar(start, goal, obstacles)  # 查找路径
+    if path:  # 找到路径则返回
         return path
     else:
-        return None  #未找到路径范围None
-    
-# 示例运行
+        return None  # 未找到路径范围None
 
-# with open('4.txt', 'r') as file:
-#     data = file.readlines()
-#     game_map = []
-#     for line in data:
-#         game_map.append(list(line))
-# obstacles=[]        
-# mapp=np.full((len(game_map), len(game_map)), 0)
-# for i in range(len(game_map)):
-#     for j in range(len(game_map)):
-#         if game_map[i][j]=='#':
-#             mapp[i][j]=1
-#             obstacle=Node(0.25+(j-1)*0.5,0.25+(100-i-1)*0.5)
-#             obstacles.append(obstacle)
-    
-# start = Node(0.75, 48.75)
-# goal = Node(49.25,0.75)
-# # obstacles = [Node(10.25, 10.25),Node(10.25, 15.25),Node(10.25, 20.25),Node(10.25, 25.25),Node(10.25, 5.25),Node(10.25, 4.25),
-#              # Node(22.25, 47.25),Node(22.25, 43.25),Node(22.25, 39.25),Node(22.25, 35.25),Node(22.25, 31.25),Node(22.25, 27.25)]
-# path = astar(start, goal, obstacles)
-# if path:
-#     path_finall=np.full((len(path), 2), 0.0)
-    
-#     i=0
-#     for node in path:
-#         print(node.x, node.y)
-#         path_finall[i][0]=node.x
-#         path_finall[i][1]=node.y
-#         i+=1
-            
-            
-    
-       
-    
-#     fig, ax = plt.subplots()
-#     ax.set_xlim(0, MAP_SIZE[0])
-#     ax.set_ylim(0, MAP_SIZE[1])   
-    
 
-#     plt.plot(path_finall[:, 0], path_finall[:, 1],linewidth=4)
-    
-    
+
+
+
+
+# def is_valid(x, y, obstacles,R):
+
+#     if x < 0.25 or x > 49.75 or y <0.25 or y > 49.75:
+#         return False
 #     for obstacle in obstacles:
-#         # plt.scatter(obstacle.x, obstacle.y, color='red')
-#         plt.gca().add_patch(Rectangle((obstacle.x-0.25, obstacle.y-0.25), 0.5, 0.5, color='red'))
+#         if (x > obstacle.x - R-0.04 and x < obstacle.x + R+0.04 and 
+#             y > obstacle.y - R-0.04 and y < obstacle.y + R+0.04):
+#             return False
+#     return True
+
+
+def get_next(current,obstacles):
+    neighbors=[]
+    for x in range(-1, 2):
+        for y in range(-1, 2):
+            
+            if x *y!=0:
+                continue
+            if current.x+x < 0.25 or current.x+x >49.75 or current.y+y < 0.25 or current.y+y >49.75 or Node(current.x+x/4,current.y+y/4) in obstacles:
+                continue
+            neighbor = Node(current.x+x/2,current.y+y/2)
+            neighbors.append(neighbor)
+    return neighbors
+
+
+
+
+def find_live_points(start, obstacles):
+    open_set = set()
+    closed_set = set()
+    current = start
+    open_set.add(current)
+    count=0
+    while open_set:
+        count+=1
+        current = min(open_set, key=lambda x: x.g + x.h)
+    
+        open_set.remove(current)
+        closed_set.add(current)
+        
+        for neighbor in get_next(current, obstacles):
+               
+            if neighbor in closed_set:
+                continue
+            
+            if neighbor not in open_set:
+                open_set.add(neighbor)
+    return closed_set
+
+
+
+
+def set_oflivepoints(map_str):
+    
+    
+    obstacles =set()
+
+    for i in range(len(map_str)):
+        for j in range(len(map_str)):
+            if map_str[i][j] == '#':  # 如果为‘#’，则为障碍物
+                x=0.25 + j * 0.5
+                y=(99.5 - i) * 0.5
+                obstacle = Node(x, y)
+                iscarry=1
+                obstacles.add(obstacle)
+                if iscarry==0:
+                    for xx in range(-2,3):
+                        for yy in range(-2,3):
+                            if (xx==0 and yy==0 ):
+                                continue
+                            obstacles.add(Node(x+xx/4,y+yy/4))
+                
+                if iscarry==1:
+                    for xx in range(-3,4):
+                        for yy in range(-3,4):
+                            if (xx==0 and yy==0 or abs(xx*yy)>=6 ):
+                                continue
+                            obstacles.add(Node(x+xx/4,y+yy/4))
+                            
+            elif map_str[i][j]=='.':
+                start_point=Node(0.25 + j * 0.5, (99.5 - i) * 0.5)   
+
+    
+    
+    start=start_point
+       
+    # start=Node(start_x,start_y)
+    
+    live_poins=find_live_points(start,obstacles)
+    
+    return live_poins
+
+
+
+def is_live(live_points,pos_x,pos_y):
+    pos=Node(pos_x,pos_y)
+    if pos in live_points:
+        return True
+    else:
+        return False
     
 
-#     plt.show()
 
-# else:
-#     print("No path found.")
+
+
+
+
+
+
+
--- control.py
+++ control.py
@@ -2,6 +2,8 @@ import numpy as np
 import sys
 import math
 
+bot_location = np.zeros(shape=(4, 2))
+
 
 def robot_control(order, _id, value=None):  # 定义了一个名为robot_control的函数，输入指令，机器人id ，参数即可操控机器人
     if value is not None:
@@ -11,54 +13,42 @@ def robot_control(order, _id, value=None):  # 定义了一个名为robot_control
 
 
 def control_to_goal(Single_robot, path, bot0_status: object):
-    if path[0][0]==0:
+    # 防止撞死：
+    # if np.linalg.norm(bot_location[Single_robot.id - 1] - np.array([Single_robot.x, Single_robot.y])) < 0.01:
+    #     robot_control("forward",Single_robot.id-1,-2)
+    #     return bot0_status
+    bot_location[Single_robot.id - 1] = np.array([Single_robot.x, Single_robot.y])
+    
+    if path[0][0] == 0:
         robot_control("rotate", Single_robot.id - 1, 0)
         robot_control("forward", Single_robot.id - 1, 0)
     else:
-        
-        bot0_status=int(bot0_status)
-        # print("bot_status:",bot0_status, file=sys.stderr)
-        target1 = np.array([path[min(len(path)-1,bot0_status)][0], path[min(len(path)-1,bot0_status)][1]])
-        # print(target1, file=sys.stderr)
-        # print(Single_robot.pos, file=sys.stderr)
+
+        bot0_status = int(bot0_status)
+        target1 = np.array([path[min(len(path) - 1, bot0_status)][0], path[min(len(path) - 1, bot0_status)][1]])
         direction1 = (target1 - Single_robot.pos) / np.linalg.norm(target1 - Single_robot.pos)  # 计算机器人到目标点的方向向量
         direction_right1 = math.atan2(direction1[1], direction1[0])  # 机器人到目标点的夹角
         ori = Single_robot.toward  # 机器人当前朝向
         dis = np.linalg.norm(Single_robot.pos - target1)  # 计算机器人到目标点的距离
-        # print(Single_robot.pos, file=sys.stderr)
-        # print(target1, file=sys.stderr)
         err = direction_right1 - ori  # 计算机器人到目标点的夹角和机器人当前朝向的误差
         while err > math.pi:
             err -= 2.0 * math.pi
         while err < -math.pi:
             err += 2.0 * math.pi
-        # print(bot0_status,file = sys.stderr)
-        # print(err,file = sys.stderr)
-    
+
         if dis < 0.25:
             robot_control("rotate", Single_robot.id - 1, 0)
             robot_control("forward", Single_robot.id - 1, 0)
             bot0_status = bot0_status + 1
-            
-        if abs(err) > math.pi/18:
-            robot_control("forward", Single_robot.id-1, 0.15)
-            robot_control("rotate", Single_robot.id-1, err*2)
-        else:   
-            r_sei=math.pi/2-(err)  
-            r=dis/2/math.cos(r_sei)
-            robot_control("forward", Single_robot.id-1, 4)
-            
-            robot_control("rotate",Single_robot.id-1,4/r)    
-    
-        # if dis < 0.2:
-        #     robot_control("rotate", Single_robot.id - 1, 0)
-        #     robot_control("forward", Single_robot.id - 1, 1)
-        #     bot0_status = bot0_status + 1
-        # else:
-        #     if np.fabs(err) > np.pi/32:
-        #         robot_control("rotate", Single_robot.id - 1, err*4)
-        #         robot_control("forward", Single_robot.id - 1, 3)
-        #     else:
-        #         robot_control("forward", Single_robot.id - 1, 5)
-        #         robot_control("rotate", Single_robot.id - 1, 0)
+
+        if abs(err) > math.pi / 18:
+            robot_control("forward", Single_robot.id - 1, 0.2)
+            robot_control("rotate", Single_robot.id - 1, err * 2)
+        else:
+            r_sei = math.pi / 2 - err
+            r = dis / 2 / math.cos(r_sei)
+            robot_control("forward", Single_robot.id - 1, 4)
+
+            robot_control("rotate", Single_robot.id - 1, 6 / r)
+
     return bot0_status
